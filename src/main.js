cd90128e19de389cf0be59a2f23dd86dd7d1fdfa
miletic94 <mileticnemanja.lab@gmail.com>
2025-03-03 15:13:08 +0100

    Add player

--- src/main.js
+++ src/main.js
@@ -11,18 +11,36 @@ const speedDown = 300;
 class GameScene extends Scene {
   constructor() {
     super('scene-game')
+    this.player;
+    this.cursor;
+    this.playerSpeed = speedDown + 50;
   }
 
   preload() {
     this.load.image('bg', '/assets/bg.png');
+    this.load.image('basket', '/assets/basket.png');
   }
   
   create() {
     this.add.image(0, 0, 'bg').setOrigin(0, 0);
+    this.player = this.physics.add.image(0, sizes.height - 100, 'basket').setOrigin(0, 0);
+    this.player.setImmovable(true);
+    this.player.body.allowGravity = false;
+    this.player.setCollideWorldBounds(true);
+
+    this.cursor = this.input.keyboard.createCursorKeys();
   }
 
   update() {
+    const {left, right} = this.cursor;
 
+    if (left.isDown) {
+      this.player.setVelocityX(-this.playerSpeed)
+    } else if (right.isDown) {
+      this.player.setVelocityX(this.playerSpeed)
+    } else {
+      this.player.setVelocityX(0);
+    }
   }
 }
 
